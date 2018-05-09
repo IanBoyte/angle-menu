af7fa3ba77f7d90f469803f0c3275238b2386c7b
Ian Boyte <ian@launchxd.com>
2018-05-09 13:24:25 -0400

    Wedge menu.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/css/app.css';
+
+import RocketImg from './images/Rocket.svg';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      navOpen: false
+    }
+
+    this.toggleNav = this.toggleNav.bind(this)
+  }
+
+  toggleNav() {
+    this.setState({navOpen: !this.state.navOpen});
+  }
+
   render() {
+    var menuClass = this.state.navOpen ? "menu-wrapper open" : "menu-wrapper";
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="nav-container">
+          <div className={menuClass}>
+            <ul >
+              <li><span>Home</span></li>
+              <li>Work</li>
+              <li>Services</li>
+              <li>About</li>
+              <li>Resources</li>
+              <li>Contact</li>
+              <li></li>
+            </ul>
+            <div className="nav-rocket"><img src={RocketImg} /></div>
+          </div>
+          <button className="nav-button" onClick={this.toggleNav} >MENU</button>
+        </div>
+        <div className="page-content">
+          <h1>BIG WORDS</h1>
+        </div>
       </div>
     );
   }
